f1d53b1ec4e3178ecb0383136647735fed92e8e1
smb123w64gb <smb123w64gb@gmail.com>
2024-10-10 06:19:50 -0700

    lets vgt

--- olkUI.py
+++ olkUI.py
@@ -4,6 +4,7 @@ default_olkName = ['human.olk','stage.olk','cdata.olk','cpudata.olk','motinfo.ol
 entry_count = [3,7,8,9,20]
 
 def changeOLK(e):
+    print(e)
     file_lst.delete(0,'end')
     idx = -1
     for item in olk_lst.curselection():
@@ -11,7 +12,17 @@ def changeOLK(e):
 
     for x in range(entry_count[idx]):
         file_lst.insert(-1,x)
-    file_lst.config(yscrollcommand=scrollbar_file.set(0,0))
+    file_lst.yview("moveto", 0)
+
+def changePKG(e):
+    pkg_lst.delete(0,'end')
+    idx = -1
+    for item in file_lst.curselection():
+      idx = item
+
+    for x in range(entry_count[idx]):
+        pkg_lst.insert(-1,x)
+    pkg_lst.yview("moveto", 0)
 
 
 root = Tk()
@@ -30,6 +41,12 @@ scrollbar_file.pack(side=RIGHT, fill=Y)
 file_lst = Listbox(root, yscrollcommand=scrollbar_file.set)
 file_lst.pack(side=LEFT, fill=BOTH)
 scrollbar_file.config(command=file_lst.yview)
+
+scrollbar_pkg = Scrollbar(root)
+scrollbar_pkg.pack(side=RIGHT, fill=Y)
+pkg_lst = Listbox(root, yscrollcommand=scrollbar_pkg.set)
+pkg_lst.pack(side=LEFT, fill=BOTH)
+scrollbar_pkg.config(command=file_lst.yview)
 
 
 mainloop()
