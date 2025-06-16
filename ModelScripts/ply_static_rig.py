fe379ce8445f27c81d210b3ce4794e0a047cd8f4
smb123w64gb <smb123w64gb@gmail.com>
2025-06-15 20:53:24 -0700

    ITS ALIVE!

--- ModelScripts/ply_static_rig.py
+++ ModelScripts/ply_static_rig.py
@@ -4,6 +4,25 @@ import copy
 import os
 import mathutils
 
+import math
+
+def euler_to_degrees(roll, pitch, yaw):
+    roll_deg = float(roll) * 360.0
+    pitch_deg = float(pitch)* 360.0
+    yaw_deg = float(yaw)* 360.0
+    return roll_deg, pitch_deg, yaw_deg
+
+def degrees_to_radians(roll, pitch, yaw):
+    roll_rad = float(roll) * (math.pi / 180.0)
+    pitch_rad = float(pitch) * (math.pi / 180.0)
+    yaw_rad = float(yaw) * (math.pi / 180.0)
+    return roll_rad, pitch_rad, yaw_rad
+
+def radians_to_degrees(roll, pitch, yaw):
+    roll_rad = 180.0 * float(roll) / (math.pi)
+    pitch_rad = 180.0 * float(pitch) / (math.pi)
+    yaw_rad = 180.0 * float(yaw) / (math.pi)
+    return roll_rad, pitch_rad, yaw_rad
 
 
 class Model(object):
@@ -67,18 +86,31 @@ for y in files:
 bone_idxes_fixup = [3,1,12,13,14,15,17,18,19,4,6,7,8,10,11]
 
 def applyTransform(vertex,bone_idx,bonez):
-    fin_trans = [0.0,0.0,0.0]
-    fin_rot = [0.0,0.0,0.0]
     next_bone = bone_idx
+    transforms = mathutils.Vector((vertex[0],vertex[1],vertex[2]))
+    chain = []
     while(next_bone != 255):
         bon = bonez[next_bone]
-        fin_trans = [fin_trans[0]+bon.StartPositionXYZScale[0],fin_trans[1]+bon.StartPositionXYZScale[1],fin_trans[2]+bon.StartPositionXYZScale[2]]
-        fin_rot = [fin_rot[0]+bon.Rotation[0],fin_rot[1]+bon.Rotation[1],fin_rot[2]+bon.Rotation[2]]
+        
+        chain.append(next_bone)
         next_bone = bon.BoneParentIdx
-
-    transform = [vertex[0]-fin_trans[0], vertex[1]-fin_trans[1], vertex[2]-fin_trans[2]]
-
-    return transform
+    chain.reverse()
+    for x in chain:
+        bon = bonez[x]
+        
+        d = euler_to_degrees(bon.Rotation[0],bon.Rotation[1],bon.Rotation[2])
+        e = degrees_to_radians(d[0],d[1],d[2])
+        e = [x for x in e]
+        r = mathutils.Euler((e[0],e[1],e[2]))
+        r = r.to_matrix()
+        r.invert()
+        
+        tra = mathutils.Vector((-bon.StartPositionXYZScale[0],-bon.StartPositionXYZScale[1],-bon.StartPositionXYZScale[2]))
+        transforms = transforms + tra
+        transforms.rotate(r)
+        
+    loc = transforms
+    return (loc[0],loc[1],loc[2])
 
 mdl_file = open(sys.argv[2], "rb")
 
@@ -99,8 +131,8 @@ mdl.materials = mdl.materials[:1]
 mdl.Object_0 = []
 for idx,x in enumerate(ply_mdls):
     mdl_lay = x.toVMX()
-    #for y in mdl_lay.StaticVerts:
-    #    y.Position = applyTransform(y.Position,bone_idxes_fixup[idx],mdl.boneInfo)
+    for y in mdl_lay.StaticVerts:
+        y.Position = applyTransform(y.Position,bone_idxes_fixup[idx],mdl.boneInfo)
     mdl_lay.MaterailIndex = 0
     mdl_lay.MatrixIndex = bone_idxes_fixup[idx]
     mdl.Object_0.append(mdl_lay)
