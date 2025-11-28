471f4ea5c616883b2b4a706372c3e277d7cb4253
smb123w64gb <smb123w64gb@gmail.com>
2025-11-27 23:59:32 -0800

    Motion in the hair

--- PkgScripts/par.py
+++ PkgScripts/par.py
@@ -12,21 +12,29 @@ def rR(f,o,l):#Read n Return, Takes file,offset,size returns data
     d = f.read(l)
     f.seek(c)
     return d
-
+def getString(f,offset = None):
+        if(offset is not None):
+            ret = f.tell()
+            f.seek(offset)
+        result = ""
+        tmpChar = f.read(1)
+        while ord(tmpChar) != 0:
+            result += tmpChar.decode("utf-8")
+            tmpChar = f.read(1)
+        if(offset):
+            f.seek(ret)
+        return result
+stringz = []
 class PAR(object):
     def __init__(self):
         self.files = []
     def read(self,f):
         magic = f.read(3)
         subver = u8(f)
-        if(subver == 2):
-            ver = u32(f)
-        else:
-            ver = u8(f)
+        ver = u32(f)
         align = 0x20 if ver == 2 else 0x80
         count = u32(f)
         smth = u32(f)
-        f.seek(0x10 % f.tell())
         mappings = []
         for _a in range(count):
             mappings.append(u32(f))
@@ -38,7 +46,17 @@ class PAR(object):
         sizes = []
         for a in range(count):
             sizes.append(mappings[a+1]-mappings[a])
+        print(sizes)
         mappings.pop()
+        if(ret%0x10):
+            f.seek(ret+(0x10-(ret%0x10)))
+        curStr = f.tell()
+        global stringz
+        for x in range(count):
+        
+            stringz.append(getString(f))
+            curStr += 0x80
+            f.seek(curStr)
         for idx,a in enumerate(mappings):
             f.seek(a)
             self.files.append(f.read(sizes[idx]))
@@ -49,6 +67,6 @@ pkg_file.close()
 outDir = str(sys.argv[1]+"_Extract/")
 os.makedirs(outDir, exist_ok=True)
 for idx,x in enumerate(pkg_in.files):
-    fil = open(outDir + str("%04i" % idx) + ".bin",'wb')
+    fil = open(outDir + stringz[idx],'wb')
     fil.write(x)
     fil.close()
